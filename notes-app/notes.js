baa91c679bcd75145e98a34c906dc8fcd5401d7e
Michael Torres <michaelj.torres@yahoo.com>
2019-04-15 15:07:14 -0400

    first challenge completed node.js

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,3 +1,16 @@
-require
 
-fs.writeFileSync('notes-txt', 'This file was created by Node.js')
+//fs.writeFileSync('notes-txt', 'My name is Mike')
+
+// 
+// Challenge: Append a message to notes.text
+//
+// 1. Use AppendFileSync to append to the file
+// 2. Run the Script
+// 3. Check your work by opening the file and viewing the appended text.
+
+const fs = require('fs')
+//fs.writeFileSync('notes.txt','Hey Mike' )
+
+fs.appendFileSync('notes.txt', ' How you doing!')
+
+
