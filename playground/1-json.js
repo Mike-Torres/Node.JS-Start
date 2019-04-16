3c9492bcb338c1fb4d9e9da3d4fcfa1404bf84c2
Michael Torres <michaelj.torres@yahoo.com>
2019-04-16 17:41:16 -0400

    challenge JSON complete

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,3 +1,25 @@
+// Challenge: Work with JSON and the file system
+//
+// 1. Load and Parse the JSON Data
+    const fs = require('fs')
+// 2. Change the name and age property using your info
+    const person = {
+        name: "Anthony",
+        planet: "Earth",
+        age: 33
+    }
+// 3. Stringify the changed object and overwrite the original data
+    const change = JSON.stringify(person)
+    fs.writeFileSync('1-json.json', change)
+    const information = fs.readFileSync('1-json.json')
+    const fileName = information.toString()
+    const data = JSON.parse(fileName)
+// 4. Test your work by viewing data in the JSON file
+     console.log(data.age)
+//
+//
+
+/*
 const fs = require('fs')
 // Read the file 
 const dataBuffer = fs.readFileSync('1-json.json')
@@ -7,7 +29,7 @@ const dataJSON = dataBuffer.toString()
 const data = JSON.parse(dataJSON)
 
 console.log(data.title)
-
+*/
 
 /*const book = {
     title: 'Ego is the Enemy',
