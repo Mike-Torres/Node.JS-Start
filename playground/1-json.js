9e7df18fea20eb7682bfd398655d65410b1edddf
Michael Torres <michaelj.torres@yahoo.com>
2019-04-16 17:20:28 -0400

    Read File/Convert file to string in JS/parsing JSON data into objects and accessing them

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,4 +1,25 @@
-const book = {
+const fs = require('fs')
+// Read the file 
+const dataBuffer = fs.readFileSync('1-json.json')
+// converted file into a string in JS
+const dataJSON = dataBuffer.toString()
+// Parsed JSON data into object and accessed property in it
+const data = JSON.parse(dataJSON)
+
+console.log(data.title)
+
+
+/*const book = {
     title: 'Ego is the Enemy',
     author: 'Ryan Holiday',
 }
+
+const bookJSON = JSON.stringify(book)
+fs.writeFileSync('1-json.json', bookJSON)
+*/
+
+
+/*
+const parsedData = JSON.parse(bookJSON)
+console.log(parsedData.author)
+*/
